70878a91cd04de11678f60f7ab21fbcc45b3f88e
anirbansil <86132826+anirbansil@users.noreply.github.com>
2024-04-10 01:43:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Plugin Name: Testimonials
+* Plugin URI: 
+* Description: Testimonials.
+* Version: 0.1
+* Author: Anirban Sil
+* Author URI: 
+* License: 
+* Text Domain: testimonial
+**/
+
+
+require_once plugin_dir_path(__FILE__) . 'includes/functions.php';
+?>
